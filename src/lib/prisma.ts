859d291ec2463983fec067ce7e1fa519eb50aa4c
kaiolincoln <kaiolincoln2001@hotmail.com>
2025-11-27 16:01:16 -0300

    fix login logoff

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client' 
 
 let prisma: PrismaClient;
 
@@ -9,13 +9,11 @@ if(process.env.NODE_ENV === "production"){
         prisma: PrismaClient;
     }
 
-
     if(!globalWithPrisma.prisma){
         globalWithPrisma.prisma = new PrismaClient();
     }
 
     prisma = globalWithPrisma.prisma;
-
 }
 
 export default prisma;
